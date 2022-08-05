a7f2e9aa1a9fd0315c72d3b8c182deb738d41672
Aankirz <sahuankit453@gmail.com>
2022-08-05 22:36:47 +0530

    ok

---
+++ fish.js
@@ -0,0 +1,30 @@
+const fish=[{
+    name:'Betta',
+    characteristics:['fairly intelligent'],
+    //diet
+    diet:[
+        //protein requirements in percentage
+       ' protein#dietary protein level in betta fish should be around 35%, and can be a mix of plant and animal proteins',
+       'Bettas have to drag along an ornate tail, which requires more calories',
+       'best betta fish diet#include a variety of freeze-dried foods, live foods (mosquito larvae, brine shrimp, bloodworms), and fish flakes and/or pellets that are high in crude protein.' 
+    ],
+     //fact
+    best:'Betta fish prefer water in the pH range of 6.5-7.5,Ammonia should ideally be zero parts per million (ppm).',
+    notBest:'No Nitrite should be present(zero parts per million (ppm)).',
+    //image
+   image:'./Betta.png'
+},{
+    name:'Goldfish',
+    characteristics:['gregarious, displaying schooling behavior'],
+    //diet
+    diet:[
+        //protein requirements in percentage
+       'There are no restrictions in the diet of an adult Goldfish as compared to their babies.',
+       
+    ],
+     //fact
+    best:'Live foods have protein, so you can feed the following live foods to them for increasing the vibrant color of their body.    ',
+    notBest:'Using Small Bowls for Homes Improperly,Giving Them Too Much Food,When and How to Clean the Tank,Reaching for the Medicine Cabinet Before the Siphon,Mixing Goldfish with Other Fish,Not Cycling the Tank First',
+    //image
+   image:'./goldfish.png'
+},]
